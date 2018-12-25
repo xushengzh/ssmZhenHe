228294b9155f1611373b597daa52247acb320783
xushengzh <xusheng_z@126.com>
2018-12-25 16:16:16 +0800

    整合加入页面，页面展示数据库的内容

--- src/main/java/com/dist/controller/BaseController.java
+++ src/main/java/com/dist/controller/BaseController.java
@@ -1,13 +1,42 @@
 package com.dist.controller;
 
+import com.dist.entity.BaseEntity;
+import com.dist.services.BaseService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.deploy.net.HttpResponse;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 /**
  * Created by Administrator on 2018/12/24.
  */
 
 @Controller
+@RequestMapping("/user")
 public class BaseController {
 
+       @Resource
+       private BaseService baseService;
 
+       @RequestMapping("/showUser")
+       public void selectUser(HttpServletRequest request, HttpServletResponse response) throws IOException {
+                   request.setCharacterEncoding("utf-8");
+                   response.setCharacterEncoding("utf-8");
+                  long userId =  Long.parseLong(request.getParameter("id"));
+                  BaseEntity baseEntity = baseService.selectUser(userId);
+                  ObjectMapper objectMapper = new ObjectMapper();
+                  response.getWriter().write(objectMapper.writeValueAsString(baseEntity));
+
+                  response.getWriter().close();
+
+
+       }
 }
